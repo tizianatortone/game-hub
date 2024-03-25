260789403e00167453ffe875fc2f4d454be4994f
tizianatortone <tiziana_tortone@hotmail.com>
2024-03-25 18:40:26 +0100

    Refactoring entities

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Game } from "../entities/Game";
 import PlatformIconList from "./PlatformIconList";
 import CriticiScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
